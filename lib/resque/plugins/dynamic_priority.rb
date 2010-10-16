a0a4f0c9c70f782f8bc8cbb6db36d0e9cf0b1602
Josh Harvey <joshmh@gmail.com>
2010-10-16 03:58:49 +0200

    initial dev

--- lib/resque/plugins/dynamic_priority.rb
+++ lib/resque/plugins/dynamic_priority.rb
@@ -1,53 +1,78 @@
 module Resque
   module Plugins
-    module DynamicPriority
-      def set_queue_priority(queue, score)
-        queue_group = get_queue_group(queue)
-        redis.zadd("queue_group:#{queue_group}", score, queue) if queue_group
-      end
+    module DynamicPriority      
+      class ClosedQueueError < RuntimeError; end
 
-      # Adds queue to priority group. Call this once all items have been enqueued.
-      # This will active the priority queue.
-      def prioritize(queue_group, queue)
-        # Note: The call order is important here. If set_queue_priority was called
-        # first, it would be possible to empty out the queue via pop, without realizing
-        # that it's a priority queue, and so the queue would never get deleted.
-        # Since there is no access to the queue until set_queue_priority is set
-        # adding the queue to the group hash first can have no ill effect.
-        redis.hset('queue-group-lookup', "queue: #{queue}", queue_group)
-        set_queue_priority(queue, 0)
-      end
+      module Base
 
-      def queues(queue_group)
-        # No need to return all queues, since empty queues are actively removed
-        redis.zrange("queue_group:#{queue_group}", 0, 5)        
-      end
+        extend self
+        
+        def update_priority(queue)
+          priority = redis.hincrby('queue-work-done', queue, 1)
+          start_time = redis.hget('queue-start-time', queue).to_i
+          delta = Time.now.to_i - start_time
+          puts "Updating priority to: #{priority.to_f / delta}" # DEBUG
+          set_queue_priority(queue, priority.to_f / delta)
+        end
+        
+        def set_queue_priority(queue, score)
+          queue_group = get_queue_group(queue)
+          redis.zadd("queue_group:#{queue_group}", score, queue) if queue_group
+        end
 
-      def get_queue_group(queue)
-        redis.hget('queue-group-lookup', "queue: #{queue}")
-      end
+        def remove_priority_queue(queue)
+          # These are extra steps necessary to remove a priority queue,
+          # Redis.remove_queue should still be called, as well.
+          if queue_group = redis.hdel('queue-group-lookup', queue)
+            redis.zrem("queue_group:#{queue_group}", queue)
+            redis.hdel('queue-work-done', queue)
+            redis.hdel('queue-start-time', queue)
+          end
+        end
+        
+        # Adds queue to priority group. Call this once all items have been enqueued.
+        # This will active the priority queue.
+        def prioritize(queue_group, queue)
+          # Note: The call order is important here. If set_queue_priority was called
+          # first, it would be possible to empty out the queue via pop, without realizing
+          # that it's a priority queue, and so the queue would never get deleted.
+          # Since there is no access to the queue until set_queue_priority is set
+          # adding the queue to the group hash first can have no ill effect.
+          redis.hset('queue-group-lookup', queue, queue_group)
+          redis.hset('queue-start-time', queue, Time.now.to_i)
+          set_queue_priority(queue, 0)
+        end
+
+        def queues(queue_group)
+          # No need to return all queues, since empty queues are actively removed
+          queue_group = queue_group[1..-1]  # lop off @
+          redis.zrange("queue_group:#{queue_group}", 0, 5)  
+        end
+
+        def get_queue_group(queue)
+          redis.hget('queue-group-lookup', queue)
+        end
       
-      def redis
-        Redis.redis
+        def redis
+          ::Resque.redis
+        end
       end
       
       module Resque
-        alias_method :dynamic_priority_original_pop, :pop
         def pop(queue)
           # Priority queues only become active after all items have been added.
           # Therefore there are no race conditions involved when checking for an empty
           # priority queue, since once it's empty it will remain empty forever.
-          item = dynamic_priority_original_pop(queue)
-          
+          item = super(queue)
+
           if item.nil?
             # If queue is empty and it's a priority queue, remove the queue.
             # This is necessary because queues are dynamically created and we
             # want all existing queues to be active. Otherwise we'd be constantly
             # processing empty queues, and the number of queues would grow forever.
-            queue_group = Resque::Plugins::DynamicPriority.get_queue_group(queue)
+            queue_group = Plugins::DynamicPriority::Base.get_queue_group(queue)
             if queue_group
               # Queue is a priority queue, since it belongs to a group, so it's safe to remove
-              redis.zrem("queue_group:#{queue_group}", queue)
               remove_queue(queue)
             end
           end
@@ -55,43 +80,29 @@ module Resque
           item
         end
         
-        alias_method :dynamic_priority_original_push, :push
         def push(queue, item)
           # If it's a priority queue, pushing is only allowed before it's activated
-          raise "Attempted to push to the activated priority queue: #{queue}" if
-            Resque::Plugins::DynamicPriority.get_queue_group(queue)
-          dynamic_priority_original_push(queue, item)
-        end
-      end
-      
-      module Worker
-        alias_method :dynamic_priority_original_queues, :queues
-        def queues
-          if @queues[0].start_with? '@'
-            # It's a queue group
-            Resque::Plugins::DynamicPriority.queues(@queues[0])
-          else
-            dynamic_priority_original_queues
-          end
-        end
-      end
-      
-      module DynamicPriorityJob
-        def after_enqueue
-          Resque::Plugins::DynamicPriority.touch_queue_priority(queue_from_class)
-        end
-
-        def around_perform(*args)
-          queue = queue_from_class
-          Resque::Plugins::DynamicPriority.set_queue_priority(queue, optimistic_score)
-          yield *args
-          Resque::Plugins::DynamicPriority.set_queue_priority(queue, accurate_score)
+          raise Plugins::DynamicPriority::ClosedQueueError,
+            "Attempted to push to the activated priority queue: #{queue}" if
+            Plugins::DynamicPriority::Base.get_queue_group(queue)
+          super
         end
         
-        def queue_from_class
-          klass = self.class
-          klass.instance_variable_get(:@queue) ||
-            (klass.respond_to?(:queue) and klass.queue)
+        def remove_queue(queue)
+          Plugins::DynamicPriority::Base.remove_priority_queue(queue)
+          super
+        end
+      end
+      
+      class PriorityWorker < ::Resque::Worker
+        def queues
+          ::Resque::Plugins::DynamicPriority::Base.queues(@queues.first)
+        end
+      end
+      
+      module DynamicPriorityJob        
+        def before_perform_update_priority(*args)
+          ::Resque::Plugins::DynamicPriority::Base.update_priority(queue)
         end        
       end
     end
@@ -99,4 +110,3 @@ module Resque
 end
 
 Resque.extend Resque::Plugins::DynamicPriority::Resque
-Resque::Worker.extend Resque::Plugins::DynamicPriority::Worker
--- test/dynamic_priority_test.rb
+++ test/dynamic_priority_test.rb
@@ -1,13 +1,21 @@
-dir = File.dirname(File.expand_path(__FILE__))
-$LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
-
-require 'test/unit'
-require 'rubygems'
-require 'turn'
-require 'resque'
-require 'resque/plugins/dynamic_priority'
+require File.dirname(__FILE__) + '/test_helper'
 
 class DynamicPriorityTest < Test::Unit::TestCase
+  include Resque::Helpers
+  
+  class SampleWorker
+    extend Resque::Plugins::DynamicPriority::DynamicPriorityJob
+    
+    def self.perform
+      # Don't need to actually do anything
+    end    
+  end
+  
+  def setup
+    Resque.redis.namespace = "resque-dynamic-priority:test"
+    Resque.redis.flushall
+  end
+  
   def test_lint
     assert_nothing_raised do
       Resque::Plugin.lint(Resque::Plugins::DynamicPriority)
@@ -21,9 +29,61 @@ class DynamicPriorityTest < Test::Unit::TestCase
   end
 
   # Need to run all Resque tests after plugin is patched in
+  def test_push_to_new_active_queue
+    Resque::Plugins::DynamicPriority::Base.prioritize('group1', 'queue1')
+    assert_raise(Resque::Plugins::DynamicPriority::ClosedQueueError) do
+      Resque.push('queue1', 'item1')
+    end
+  end
 
-  def test_override
-    Resque.pop 'bogus'
+  def test_push_to_existing_active_queue
+    assert_nothing_raised { Resque.push('queue1', 'item1') }
+    Resque::Plugins::DynamicPriority::Base.prioritize('group1', 'queue1')
+    assert_raise(Resque::Plugins::DynamicPriority::ClosedQueueError) do
+      Resque.push('queue1', 'item2')
+    end
+  end
+
+  def test_push
+    Resque.push('queue', 'item')
+    assert_equal 'item', decode(Resque.redis.lpop("queue:queue"))
+  end
+
+  def test_pop
+    Resque.push('queue', 'item')
+    assert_equal 'item', Resque.pop('queue')
+  end
+  
+  def test_prioritizing
+    Resque::Job.create(:slow, SampleWorker)
+    Resque::Job.create(:slow, SampleWorker)
+    Resque::Job.create(:slow, SampleWorker)
+    Resque::Job.create(:slow, SampleWorker)
+    Resque::Job.create(:slow, SampleWorker)
+    Resque::Job.create(:slow, SampleWorker)
+    Resque::Job.create(:slow, SampleWorker)
+    Resque::Job.create(:slow, SampleWorker)
+    Resque::Job.create(:slow, SampleWorker)
+    Resque::Job.create(:fast, SampleWorker)
+    Resque::Job.create(:fast, SampleWorker)
+    Resque::Job.create(:fast, SampleWorker)
+    Resque::Job.create(:fast, SampleWorker)
+    Resque::Job.create(:fast, SampleWorker)
+    Resque::Job.create(:fast, SampleWorker)
+    Resque::Job.create(:fast, SampleWorker)
+    Resque::Job.create(:fast, SampleWorker)
+    Resque::Job.create(:fast, SampleWorker)
+    Resque::Job.create(:fast, SampleWorker)
+    Resque::Plugins::DynamicPriority::Base.prioritize('group1', :slow)
+    Resque::Plugins::DynamicPriority::Base.prioritize('group1', :fast)
+    worker = Resque::Plugins::DynamicPriority::PriorityWorker.new('@group1')
+    
+    pqueues = []
+    worker.work(0) do |job|
+      pqueues << job.queue
+    end
+    
+    p pqueues
   end
 end
 
